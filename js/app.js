e939a71250e8dc0d75b270fb71eaa859b2b677c6
Mallory Eastburn <mallorykaitlin@gmail.com>
2020-08-23 09:41:59 -0500

    Adjusted JS
    Took out source code, added api to a quote JSON, added random quote generator, selected html to display quote and author.

--- js/app.js
+++ js/app.js
@@ -1,44 +1,14 @@
-//immediate invoked function expression
-(function() {
-  const quotes = [
-    {
-      quote:
-        "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
-      author: " Life"
-    },
-    {
-      quote:
-        "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
-      author: "John Kenneth Galbraith"
-    },
-    {
-      quote:
-        "God save me from my friends. I can protect myself from my enemies.",
-      author: "Claude Louis Hector de Villars "
-    },
-    {
-      quote: "The price of anything is the amount of life you exchange for it.",
-      author: "David Thoreau"
-    },
-    {
-      quote:
-        "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
-      author: "Charles Lindbergh"
-    },
-    {
-      quote:
-        "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
-      author: " Tyne Daly"
-    }
-  ];
+const apiUrl = `https://type.fit/api/quotes`;
 
-  const btn = document.getElementById("generate-btn");
+function randomQuote(response) {
+  let quote =
+    response.data[parseInt(Math.floor(Math.random() * response.data.length))];
+  document.querySelector("#quote").innerHTML = quote.text;
+  document.querySelector(".ml-3").innerHTML = quote.author;
+}
 
-  btn.addEventListener("click", function() {
-    let random = Math.floor(Math.random() * quotes.length);
-    console.log(random);
-
-    document.getElementById("quote").textContent = quotes[random].quote;
-    document.querySelector(".author").textContent = quotes[random].author;
-  });
-})();
+function quoteTime() {
+  axios.get(apiUrl).then(randomQuote);
+}
+quoteTime();
+document.querySelector("#generate-btn").addEventListener("click", quoteTime);
